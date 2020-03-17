835b08be534b9d416cc29a7749e2a38818e9d33f
unknown <general@simplexwebsites.com>
2020-03-17 13:10:38 -0400

    Update .help

--- components/help.js
+++ components/help.js
@@ -5,9 +5,7 @@ const help = (msg, discord, client)=>{
             .setColor("#0099ff")
             .setTitle("PCF Bot Help")
             .setThumbnail("https://cdn.discordapp.com/attachments/676662857831546880/687478153714860117/PCF_transparent.png")
-            .addField("ATIS Commands - Add ATIS", ".addAtis [Airport (ICAO)] [Current Info] [Runway Ops]")
-            .addField("ATIS Commands - Modify ATIS",".modifyAtis [Airport (ICAO)] [Current Info] [Runway Ops]")
-            .addField("ATIS Commands - Remove ATIS",".removeAtis [Airport (ICAO)]")
+            .addField("ATIS Commands - Update ATIS",".updateatis [Airport (ICAO)] [Current Info] [Runway Ops]")
             .addField("Wallop Report", ".wallop [mention to user] [reason]")
             .addField("Role Updater (Administrator ONLY)", ".startRoleUpdater")
             .addField("Position Updater (Administrator ONLY)", ".startpositionupdater");
